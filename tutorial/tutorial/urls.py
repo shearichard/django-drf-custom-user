76489b1eb2ca14e86390e9cd18474fee532904d5
Richard Shea <rshea@thecubagroup.com>
2024-01-11 08:26:51 +1300

    Make admin work

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -1,5 +1,8 @@
-from django.urls import path, include
+from django.urls import re_path, path, include
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
 	path('', include('snippets.urls')),
 ]
